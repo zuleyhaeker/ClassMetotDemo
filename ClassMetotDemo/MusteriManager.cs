47da8c488ff223710553d38e50e6f2cbf9e0bdc0
ekerzuleyha <66801741+ekerzuleyha@users.noreply.github.com>
2022-03-23 09:39:46 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri eklendi : " +musteri.Ad);
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri silindi : " +musteri.Ad);
+        }
+        public void Listele(Musteri[] musteriler)
+        {
+            Console.WriteLine("müşteri listesi:");
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Ad);
+            }
+        }
+       
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Musteri musteri1 = new Musteri();
+            musteri1.Ad = "Ali";
+            musteri1.Soyad = "Say";
+            musteri1.Job = "bankacı";
+            
+            Musteri musteri2 = new Musteri(); 
+            musteri2.Ad = "Veli";
+            musteri2.Soyad = "Saymak";
+            musteri2.Job = "işçi";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Ad = "Ayşe";
+            musteri3.Soyad = "Çimen";
+            musteri3.Job = "öğretmen";
+
+            Musteri musteri4 = new Musteri();
+            musteri4.Ad = "Canan";
+            musteri4.Soyad = "Can";
+            musteri4.Job = "mühendis";
+
+            Musteri musteri5 = new Musteri();
+            musteri5.Ad = "Hasan";
+            musteri5.Soyad = "Efe";
+            musteri5.Job = "Bakkal";
+
+            Musteri[] musteriler = new Musteri[]
+            {
+                musteri1,musteri2,musteri3,musteri4,musteri5
+            };
+
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Ad);
+                Console.WriteLine(musteri.Soyad);
+                Console.WriteLine(musteri.Job);
+                Console.WriteLine("------------");
+                             
+            }
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+            musteriManager.Ekle(musteri3);
+            musteriManager.Ekle(musteri4);
+            musteriManager.Ekle(musteri5);
+
+            musteriManager.Listele(musteriler);
+
+            musteriManager.Sil(musteri3);
+            musteriManager.Sil(musteri5);
+        }
+    }
+}
